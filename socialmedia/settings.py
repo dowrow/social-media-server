3abc6b9929b981517c7dbebd7f3f6b64e02dd8b8
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2016-05-17 23:34:54 +0200

    Added global pagination

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -149,6 +149,8 @@ REST_FRAMEWORK = {
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 
