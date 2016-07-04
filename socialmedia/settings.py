287e00bd617f254aaa783b371e7e487d7eeea69b
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2016-07-04 12:57:00 +0200

    debug=True

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "pvs4s(pqfx&q(#v6e)npw^-&(v)xt$h*q2k9)kq%x%a!k8hxrs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 INSTALLED_APPS = (
